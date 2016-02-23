21e352bae4877f74ced2b3667205c60715c6fc77
eLtMosen <koennecke@mosushi.com>
2016-02-23 21:49:53 +0100

    Buzzword crypto überarbeitet

--- src/BingoBundle/Resources/public/js/script.js
+++ src/BingoBundle/Resources/public/js/script.js
@@ -86,6 +86,7 @@ $(document).ready(function () {
         bindEventHandler();
         initBingoCard();
         //toggleCaptionOnOff();
+	alert("Ajax failed to fetch data");
     }
 
     function createBingoCard() {
