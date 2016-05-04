a45e07b5647099ed6d0a455dd0ff57615476c320
eltmosen <koennecke@mosushi.com>
2016-05-04 12:44:06 +0200

    wonBuzzwords array korrektur

--- src/BingoBundle/Resources/public/js/game.js
+++ src/BingoBundle/Resources/public/js/game.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     var buzzwordConfirmed = new Array(config.buzzwordCount);
     var buzzwordBusy = new Array(config.buzzwordCount);
     var wonBingos = new Array(143);
-    var wonBuzzwords = new Array(config.buzzwordCount);
+    var wonBuzzwords = new Array(0);
     var userRejected = new Array(19,88,71,59,8,55,1,20,35,7,31,57,44,4,67,86,52,89);
     var userRejectedNum = userRejected.length;
     var totalScore = 0;
@@ -552,8 +552,8 @@ $(document).ready(function () {
                             buzzwordConfirmed[entry.card] = true;
                         }
                         if (entry.clicks <= 6) {
-                            wonBuzzwords[entry] = entry;
-                            console.log(wonBuzzwords);
+                            wonBuzzwords[entry.card] = entry.card;
+                            console.log(entry.card);
                         }
                     });
 
