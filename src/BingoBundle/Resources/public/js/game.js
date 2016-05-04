fcf8cdd6284f78dbde8ca11b29d611dea1e75799
eltmosen <koennecke@mosushi.com>
2016-05-04 12:22:29 +0200

    wonBuzzwords eingeführt um beteiligung am freischalten mit 10 punkten zu belohnen

--- src/BingoBundle/Resources/public/js/game.js
+++ src/BingoBundle/Resources/public/js/game.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
     var buzzwordConfirmed = new Array(config.buzzwordCount);
     var buzzwordBusy = new Array(config.buzzwordCount);
     var wonBingos = new Array(143);
+    var wonBuzzwords = new Array(config.buzzwordCount);
     var userRejected = new Array(19,88,71,59,8,55,1,20,35,7,31,57,44,4,67,86,52,89);
     var userRejectedNum = userRejected.length;
     var totalScore = 0;
@@ -550,6 +551,10 @@ $(document).ready(function () {
                         if (entry.clicks >= 6) {
                             buzzwordConfirmed[entry.card] = true;
                         }
+                        if (entry.clicks <= 6) {
+                            wonBuzzwords[entry.card] = entry.card;
+                            console.log(wonBuzzwords);
+                        }
                     });
 
                     activateBuzzword($that, clickSource, id_img);
@@ -675,6 +680,7 @@ $(document).ready(function () {
         }, []);
         var i = 1;
         totalScore = 0;
+        totalScore = wonBuzzwords.length * 10;
         $.each(winBoards, function (key, value) {
             if (containsAll(value, convertedToNum)) {
                 wonBingos[i] = key;
